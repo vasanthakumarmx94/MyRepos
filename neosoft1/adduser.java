6cf7c1a0b7e2b194df85a1ad6c878a4dc75e888a
vasanthakumarmx94 <vasanthakumarmx94@gmail.com>
2021-09-17 16:07:00 +0530

    commit add user, delete user

---
+++ neosoft1/adduser.java
@@ -0,0 +1,11 @@
+package neosoft1;
+
+public class adduser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Add user file Welcome ");
+
+	}
+
+}
---
+++ neosoft1/deleteuser.java
@@ -0,0 +1,11 @@
+package neosoft1;
+
+public class deleteuser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Delete user file Welcome ");
+
+	}
+
+}
